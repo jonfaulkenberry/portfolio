9e7f885ca94b1f60136b8f92f6f3923e2e1db054
Jon Faulkenberry <jon.faulkenberry@outlook.com>
2014-02-08 16:05:29 -0800

    Added rack-zippy initializer file

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,6 @@ module Portfolio
     config.generators do |g|
       g.test_framework :mini_test, :spec => true, :fixture => false
     end
-    
-    config.middleware.swap(ActionDispatch::Static, Rack::Zippy::AssetServer)
   end
 end
 

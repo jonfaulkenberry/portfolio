2cafa3f5e989dc8a75c505c28bb2d66d6e3ea32f
Jon Faulkenberry <jon.faulkenberry@outlook.com>
2014-02-11 09:51:36 -0800

    Added secret_key_base to application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Portfolio
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de 
     
+    config.secret_key_base = 'faulkenberry-portfolio'
+    
     config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
     
     config.generators do |g|

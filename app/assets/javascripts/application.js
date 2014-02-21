b8705e4c1ca3f0e69a935b7a48bef839216cbf13
Jon Faulkenberry <jon.faulkenberry@outlook.com>
2014-02-20 22:49:31 -0800

    added nprogress loading indicator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,5 +5,13 @@
 //= require_tree .
 //= require_tree ../../../vendor/assets/javascripts
 //= require turbolinks
+//= require nprogress
+//= require nprogress-turbolinks 
+
+NProgress.configure({
+  showSpinner: false,
+  ease: 'ease',
+  speed: 500
+}); 
 
 $(function(){ $(document).foundation(); });

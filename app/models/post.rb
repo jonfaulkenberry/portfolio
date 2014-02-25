c0905956c6a5a157de8c820128115d533b6346fb
Jon Faulkenberry <jon.faulkenberry@outlook.com>
2014-02-25 13:01:42 -0800

    added post model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, :use => [:slugged, :history]
-
   belongs_to :author, class_name: "User"
+  validates :title, length: { in: 4..255 }
+  validates_presence_of :body, :on => :create, :message => "can't be blank"
 
   def should_generate_new_friendly_id?
     slug.blank? || title_changed?

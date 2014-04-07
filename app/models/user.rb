4d445d73ca7ebf045eec6071c7d4bf5c1d113f49
Jon Faulkenberry <jon.faulkenberry@outlook.com>
2014-04-06 18:29:05 -0700

    Made Twitter the only form of authentication possible

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts, foreign_key: "author_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable,
-    :omniauthable, :registerable, :omniauth_providers => [:twitter]
+  devise :database_authenticatable, :rememberable, :omniauthable, :omniauth_providers => [:twitter]
 
   def owner?
     role == "owner"

c793aa528c3d236b66b3a65453fb0555bce97303
Jon Faulkenberry <jon.faulkenberry@outlook.com>
2014-04-08 20:25:58 -0700

    Removed email from User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406225229) do
+ActiveRecord::Schema.define(version: 20140409032338) do
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 20140406225229) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -66,7 +65,6 @@ ActiveRecord::Schema.define(version: 20140406225229) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end

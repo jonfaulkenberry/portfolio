5fdd277ca43af72c6f35ef1c84aea4805f11734b
Jon Faulkenberry <jon.faulkenberry@outlook.com>
2014-06-13 10:33:22 -0700

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Portfolio::Application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
   
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks"  }
   resources :posts
   resources :projects
   
-
   root 'welcome#index'
   get '/about' => 'welcome#about', as: 'about'
   get '/blog' => 'posts#index', as: 'blog'
@@ -15,6 +13,9 @@ Portfolio::Application.routes.draw do
   get '/portfolio' => 'projects#index', as: 'portfolio'
   get '/search' => 'posts#search', as: 'search'
   get '/posts/by-month/:month/:year' => 'posts#posts_by_month'
+  
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+    
   devise_scope :user do
       get "/login" => "devise/sessions#new"
       get "/logout" => "devise/sessions#destroy"

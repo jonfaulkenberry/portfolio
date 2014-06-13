df47327e4c772a9aa6b17194026be1770ad8a731
Jon Faulkenberry <jon.faulkenberry@outlook.com>
2014-06-12 20:51:47 -0700

    fixed coveralls and bg image

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,12 @@
+require "coveralls"
+Coveralls.wear!('rails')
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/rails/capybara"
 require "minitest/pride"
-require "coveralls"
-
-Coveralls.wear!('rails')
 
 class ActiveSupport::TestCase
   include FactoryGirl::Syntax::Methods

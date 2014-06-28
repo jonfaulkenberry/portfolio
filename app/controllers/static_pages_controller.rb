292a8267042169becf2df5c857a929c0b5b9ee2a
Jon Faulkenberry <jon.faulkenberry@outlook.com>
2014-06-27 18:14:58 -0700

    added recaptcha

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def contact
-    if request.post?  
+    if request.post? && verify_recaptcha
       ContactFormMailer.delay.contact_form_email(params[:sender_name], params[:sender_email], params[:message])
       flash[:notice] = "Your message has been sent!"
       redirect_to root_path
